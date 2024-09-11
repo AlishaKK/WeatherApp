8bec9981694e0ed153453f2ba87ac143d4c833ce
AlishaKK <162887724+AlishaKK@users.noreply.github.com>
2024-09-12 02:50:31 +0500

    updated

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -47,7 +47,7 @@ useEffect(()=>{
         onChange={(e) => setCity(e.target.value)}
       />
       <button
-        className="search_button bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg"
+        className="search_button bg-blue-500  hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-xs"
         type="submit"
       >
         Search
