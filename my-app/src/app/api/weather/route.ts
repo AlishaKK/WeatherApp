39636ee0d0758a86bb552b809cfad28add32069e
AlishaKK <162887724+AlishaKK@users.noreply.github.com>
2024-09-12 01:11:17 +0500

    updated

---
+++ my-app/src/app/api/weather/route.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from "next/server";
+
+//localhost:3000/api/weather
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+  const address = searchParams.get("address");
+  const latitude = searchParams.get("lat");
+  const longitude = searchParams.get("lon");
+  let url = "";
+  if (address) {
+    url =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      address +
+      "&appid=" +
+      "ee74515c220a455b222ea67ead34ea91";
+  } else {
+    url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=ee74515c220a455b222ea67ead34ea91`;
+  }
+  console.log(url);
+  const res = await fetch(url);
+
+  const data = await res.json();
+  return NextResponse.json({ data });
+}
